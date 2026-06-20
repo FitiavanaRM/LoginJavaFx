698ad88b8bd9002019fc8485821e1d48b110c98f
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-06-20 18:36:43 +0300

    ajout du code dans AuthService

--- src/main/java/com/cours/app/service/AuthService.java
+++ src/main/java/com/cours/app/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.cours.app.service;
 
 public class AuthService {
+    private static final String user_name = "rotsynomena9@gmail.com";
+    private static final String pass_word = "rotsy@123456";
+
+    public boolean vraiFaux(String username, String password) {
+        return user_name.equals(username) && pass_word.equals(password);
+    }
 }
