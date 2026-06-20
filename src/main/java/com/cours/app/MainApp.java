cab3356e9f5a8a65ccc3d43b739b025077183798
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-06-20 21:38:53 +0300

    ajout du code dans mainApp

--- src/main/java/com/cours/app/MainApp.java
+++ src/main/java/com/cours/app/MainApp.java
@@ -1,4 +1,22 @@
 package com.cours.app;
 
-public class MainApp {
+import com.cours.app.iu.view;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage display) {
+        display.setTitle("Connexion");
+
+        view loginView = new view();
+        display.setScene(loginView.buildScene(display));
+        display.setResizable(false);
+        display.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
