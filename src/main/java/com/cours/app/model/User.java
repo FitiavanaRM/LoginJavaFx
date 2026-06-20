0fb94e7cf65f63b90f0548270131ad4237b15300
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-06-20 18:25:21 +0300

    ajout du code sur User.java

--- src/main/java/com/cours/app/model/User.java
+++ src/main/java/com/cours/app/model/User.java
@@ -1,4 +1,24 @@
 package com.cours.app.model;
 
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User(String password) {
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
